466e61f8eda2f8f3f424410d7053f40dc0b92047
CVSCharan <charan.cvs@gmail.com>
2022-08-07 16:56:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div class="profile-container p-5">
+        <h1>CVS CHARAN</h1>
+        <div class="">
+          <p>FRONT END DEVELOPER</p>
+          <div class="designation-details-container">
+            <p class="designation-details mt-4">
+              I am a Front-End Developer with an year of works experience across
+              the product development life cycle.
+            </p>
+          </div>
+          <div class="contact-info-container">
+            <p class="contact-info mt-4">
+              Send me an Email to get access to my work.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="todo-list-container">
+        <h1>TodoList</h1>
+        <h1 id="todayHeading">Today</h1>
+        <ul>
+          <li>Have a Drink</li>
+          <li class="completed">Go for a walk</li>
+          <li>Read a book</li>
+          <li class="completed">Schedule Meeting with Alex</li>
+        </ul>
+
+        <h1>Tomorrow</h1>
+        <ul>
+          <li>Lister to the Video Session</li>
+          <li>Read Monthly Magazine</li>
+          <li>Attempt Practice questions</li>
+          <li>Go for Shopping</li>
+        </ul>
+      </div>
+      <div class="language-container">
+        <h1 class="blue-text">JavaScript</h1>
+        <p id="description" class="language-description">
+          JavaScript is the programming language of the Web. JavaScript adds
+          interactivity to the website that engages a user.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button type="button" style={{ backgroundColor: "orange" }}>
+          Learn
+        </button>
+      </div>
     </div>
   );
 }
